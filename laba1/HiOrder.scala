ef5cdde82907e8f28e81dad59e1113faa59b35e0
killso <61009092+killso@users.noreply.github.com>
2020-06-23 15:13:31 +0300

    FIX

--- laba1/HiOrder.scala
+++ laba1/HiOrder.scala
@@ -1,1 +1,24 @@
+package exercise1
 
+/** Напишите ваши решения в виде функций. */
+object HigherOrder {
+
+  val plus: (Int, Int) => Int = _ + _
+  val multiply: (Int, Int) => Int = _ * _
+
+  /* a) Напишите функцию, которая принимает `f: (Int, Int) => Int`, параменты `a` и `b`
+   *    и коэффициент умножения `n` и возвращает n * f(a, b). Назовите `nTimes`.
+   */
+  def nTimes(f: (Int, Int) => Int, a: Int, b: Int, n: Int): Int = n * f(a, b)
+
+
+  // примените вашу функцию (a) здесь, не изменяйте сигнатуру
+  def testnTimes(f: (Int, Int) => Int, a: Int, b: Int, n: Int): Int = nTimes(f, a, b, n)
+
+  /* b) Напишите анонимную функцию, функцию без идентификатора ((a, b) => ???) для `nTimes` которая
+   *    выполняет следующее:
+   *          if (a > b) a else b
+   */
+  def anon = (a: Int, b: Int) => if (a > b) a else b
+  def Anon1(a: Int, b: Int, n: Int): Int = nTimes(anon, a, b, n)
+}
