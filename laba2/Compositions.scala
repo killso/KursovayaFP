3e9e83f6c130fbecdb975c0411eff3812db0137d
killso <61009092+killso@users.noreply.github.com>
2020-06-23 15:06:56 +0300

    Add files via upload

--- laba2/Compositions.scala
+++ laba2/Compositions.scala
@@ -10,7 +10,6 @@
  * flatMap берет функцию, которая работает с вложенными списками и объединяет результаты.
  */
 sealed trait Option[A] {
-
   def map[B](f: A => B): Option[B]
   def flatMap[B](f: A => Option[B]): Option[B]
 }
@@ -19,25 +18,21 @@ case class Some[A](a: A) extends Option[A] {
   def map[B](f: A => B): Option[B] = Some(f(a))
   def flatMap[B](f: A => Option[B]): Option[B] = f(a)
 }
-case class None[A]()     extends Option[A] {
-
+case class None[A]() extends Option[A] {
   def map[B](f: A => B): Option[B] = None()
   def flatMap[B](f: A => Option[B]): Option[B] = None()
 }
-
 /** Напишите ваши решения в тестовых функциях.  */
 object Compositions {
-
   // a) Используйте данные функции. Вы можете реализовать свое решение прямо в тестовой функции.
   // Нельзя менять сигнатуры
-
-  def testCompose[A, B, C, D](f: A => B)
-                             (g: B => C)
-                             (h: C => D): A => D = f andThen g andThen h
-
+  def testMapFlatMap[A, B, C, D](f: A => Option[B])
+                                (g: B => Option[C])
+                                (h: C => D): Option[A] => Option[D] = {
+    _.flatMap(f).flatMap(g).map(h)
+  }
   // b) Напишите функции с использованием `map` и `flatMap`. Вы можете реализовать свое решение прямо в тестовой функции.
   // Нельзя менять сигнатуры
-
   def testMapFlatMap[A, B, C, D](f: A => Option[B])
                                 (g: B => Option[C])
                                 (h: C => D): Option[A] => Option[D] = {
@@ -45,7 +40,6 @@ object Compositions {
   }
   // c) Напишите функцию используя for. Вы можете реализовать свое решение прямо в тестовой функции.
   // Нельзя менять сигнатуры
-
   def testForComprehension[A, B, C, D](f: A => Option[B])
                                       (g: B => Option[C])
                                       (h: C => D): Option[A] => Option[D] =
