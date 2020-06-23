47d847e595b67355560b575de2f3141c5b7f5451
killso <61009092+killso@users.noreply.github.com>
2020-06-23 18:06:12 +0300

    Add files via upload

--- laba3/Adts (1).scala
+++ laba3/Adts (1).scala
@@ -35,39 +35,39 @@ object Adts {
     }
   }
   // примените функцию из пункта (a) здесь, не изменяйте сигнатуру
-  def testReturnItem(list: List[Int], n: Int): Option[Int] = ReturnItem(list, n)
+  def testGetNth(list: List[Int], n: Int): Option[Int] = ReturnItem(list, n)
   // b) Напишите функцию, увеличивающую число в два раза.
-  def kvadrat(n: Option[Int]): Option[Int] = n match{
-     
-      case Some(a) => Option(a*2)
-      case None => null
 
-  }
+  def Double(n: Option[Int]): Option[Int] = if (n.isDefined)  Some(n.get * 2)  else None
+
   // примените функцию из пункта (b) здесь, не изменяйте сигнатуру
-  def testkvadrat(n: Option[Int]): Option[Int] = kvadrat(n)
+  def testDouble(n: Option[Int]): Option[Int] = Double(n)
+
   // c) Напишите функцию, проверяющую является ли число типа Int четным. Если так, верните Right. В противном случае, верните Left("Нечетное число.").
-  def EvenNumber(n: Int): Either[String, Int] = Either.cond(n%2==0, n, "Нечетное число") match
-  {
-    case Left(i)=>Left(i)
-    case Right(s)=>Right(s)
+
+  def IsEven(n: Int): Either[String, Int] = n % 2 match {
+    case 0 => Right(n)
+    case 1 => Left("Odd number")
   }
+
   // примените функцию из пункта (c) здесь, не изменяйте сигнатуру
-  def testEvenNumber(n: Int): Either[String, Int] = EvenNumber(n)
+  def testIsEven(n: Int): Either[String, Int] = IsEven(n)
+
   // d) Напишите функцию, реализующую безопасное деление целых чисел. Верните Right с результатом или Left("Вы не можете делить на ноль.").
-  def NumberDivision(a: Int, b: Int): Either[String, Int] = Try(a/b) match
-  {
-    case Success(a) => Right(a)
-    case Failure(error) => Left("Нельзя делить на 0")
+
+  def SafeDivide(a: Int, b: Int): Either[String, Int] = {
+    if (b == 0) Left("Cannot divide by zero")
+    else Right(a / b)
   }
+
   // примените функцию из пункта (d) здесь, не изменяйте сигнатуру
-  def testNumberDivision(a: Int, b: Int): Either[String, Int] = NumberDivision(a, b)
+  def testSafeDivide(a: Int, b: Int): Either[String, Int] = SafeDivide(a, b)
+
   // e) Обработайте исключения функции с побочным эффектом вернув 0.
-  def DivisionByZero(impure: String => Int, str: String): Try[Int] =
-    Try(impure(str)).toEither match
-    {
-      case Right(k)=>Success(k)
-      case Left(l)=>Success(0)
-    }
+
+  def GoodOldJava(impure: String => Int, str: String): Try[Int] = Try(impure(str))
+
   // примените функцию из пункта (e) здесь, не изменяйте сигнатуру
-  def testDivisionByZero(impure: String => Int, str: String): Try[Int] = DivisionByZero(impure, str)
+  def testGoodOldJava(impure: String => Int, str: String): Try[Int] = GoodOldJava(impure, str)
+
 }
